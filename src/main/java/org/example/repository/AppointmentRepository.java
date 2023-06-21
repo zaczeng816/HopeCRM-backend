1da5f1256b0c4ff48c0fb7871f439786968fe030
Zachary Zeng <48763165+zaczeng816@users.noreply.github.com>
2023-06-21 10:34:45 +0800

    init for spring

--- src/main/java/org/example/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- src/main/java/org/example/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ClientRepository extends JpaRepository<Client, Long> {
-
-}
